d902fcc404e79644de9603b7437f766f69314694
QA1.3 <dijana.veselinovic@yahoo.com>
2023-03-07 20:00:25 +0100

    https://github.com/Dijana81/My-app

---
+++ src/SuperClass.java
@@ -0,0 +1,11 @@
+public class SuperClass {
+    int age;
+
+    SuperClass(int age){
+        this.age = age;
+    }
+
+    public void getAge() {
+        System.out.println("Ova vrednost variable je" + age);
+    }
+}
